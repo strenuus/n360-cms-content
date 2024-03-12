6d17be8acfba71de6e1532ab8b41193df78df8d5
Noah Summers <nsmmrs@outlook.com>
2024-03-12 17:49:15 -0500

    10203 - Disable Links and Buttons in Preview Pane
    * add basic eslint config
    * fix "rules of hooks" violations in createPreview

--- src/cms/preview-templates/lib/createPreview.jsx
+++ src/cms/preview-templates/lib/createPreview.jsx
@@ -4,73 +4,75 @@ import { useState, useLayoutEffect } from 'react'
 
 export default function createPreview(component) {
   return (props) => {
-    setPreviewPaneId(props)
-    setUpFontAwesome(props)
-    ignoreLinksAndButtons(props)
-    moveStylesToPreviewPane(props)
+    const { window, document } = props
+
+    useLayoutEffect(() => {
+      const cleanUpId = setPreviewPaneId(document)
+      const cleanUpFontAwesome = setUpFontAwesome(document)
+      const cleanUpClickHandler = ignoreLinksAndButtons(document)
+
+      return () => {
+        cleanUpId()
+        cleanUpFontAwesome()
+        cleanUpClickHandler()
+      }
+    }, [document])
+
+    const mainHead = window.parent.document.head
+    const previewPaneHead = document.head
+    useMutationObserver(mainHead, moveStylesToElement(previewPaneHead));
 
     return <Shell renderApp={() => component(props)} />
   }
 }
 
-const setPreviewPaneId = ({document}) => {
-  useLayoutEffect(() => {
-    const oldId = document.body.getAttribute("id")
-    document.body.setAttribute("id", "app")
+const setPreviewPaneId = (document) => {
+  const oldId = document.body.getAttribute("id")
+  document.body.setAttribute("id", "app")
 
-    return () => document.body.setAttribute("id", oldId)
-  }, [])
+  return () => document.body.setAttribute("id", oldId)
 }
 
-const setUpFontAwesome = ({document}) => {
-  useLayoutEffect(() => {
-    const fontAwesome = document.createElement('script')
-    fontAwesome.setAttribute('src', 'https://kit.fontawesome.com/74c5cf378a.js')
-    fontAwesome.setAttribute('crossorigin', 'anonymous')
-    document.head.appendChild(fontAwesome)
+const setUpFontAwesome = (document) => {
+  const fontAwesome = document.createElement('script')
+  fontAwesome.setAttribute('src', 'https://kit.fontawesome.com/74c5cf378a.js')
+  fontAwesome.setAttribute('crossorigin', 'anonymous')
+  document.head.appendChild(fontAwesome)
 
-    return () => document.head.removeChild(fontAwesome)
-  }, [])
+  return () => document.head.removeChild(fontAwesome)
 }
 
-const ignoreLinksAndButtons = ({document}) => {
-  useLayoutEffect(() => {
-    const handler = event => {
-      if (["A", "BUTTON"].includes(event.target.tagName)) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
+const ignoreLinksAndButtons = (document) => {
+  const handler = event => {
+    if (["A", "BUTTON"].includes(event.target.tagName)) {
+      event.preventDefault()
+      event.stopPropagation()
     }
+  }
 
-    document.body.addEventListener("click", handler)
+  document.body.addEventListener("click", handler)
 
-    return () => document.body.removeEventListener("click", handler)
-  }, [])
+  return () => document.body.removeEventListener("click", handler)
 }
 
-const moveStylesToPreviewPane = ({window}) => {
+const moveStylesToElement = (element) => {
   // After importing 'start' from N360, the N360 styles are appended to the main window,
   // where they do not affect the iframe containing the preview pane (the only place they should affect).
   // Since the styles are not there initially, we need to watch for them and move them as they are created.
   // (We do want to move them, not copy them, as we do not want them affecting the main CMS interface).
 
-  const mainHead = window.parent.document.head
-  const iframeHead = window.document.head
-
   const isStylesheet = (node) => node.tagName === 'LINK' && node.rel === 'stylesheet'
   const isStyleTag = (node) => node.tagName === 'STYLE'
 
-  const onMutation = function(mutations) {
+  return function(mutations) {
     for (const mutation of mutations) {
       for (const node of mutation.addedNodes) {
         if (isStylesheet(node) || isStyleTag(node)) {
-          iframeHead.appendChild(node)
+          element.appendChild(node)
         }
       }
     }
   }
-
-  useMutationObserver(mainHead, onMutation);
 }
 
 function useMutationObserver(target, onMutation) {
