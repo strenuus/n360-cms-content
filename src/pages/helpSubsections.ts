81dc5241eb0d59c74d5ec8cf43910adb9c414af5
Noah Summers <nsmmrs@outlook.com>
2024-03-31 00:14:46 -0500

    10221 - Help Subsections Resource

---
+++ src/pages/helpSubsections.ts
@@ -0,0 +1,23 @@
+import { graphql } from "gatsby"
+
+const page = () => null
+export default page
+
+export const query = graphql`
+  query HelpSubsectionsQuery {
+    allHelpSubsectionsJson {
+      nodes {
+        sectionSlug
+        title
+        slug
+        body
+        tiles {
+          iconName
+          title
+          linkPath
+          body
+        }
+      }
+    }
+  }
+`
