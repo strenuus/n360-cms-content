421b5561d816ce57b16a9e465d5789b9fe365940
Noah Summers <nsmmrs@outlook.com>
2024-05-16 12:59:27 -0500

    Fix "// is not a valid url" issue on login screen

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,6 +26,13 @@ const config: GatsbyConfig = {
       resolve: `gatsby-plugin-decap-cms`,
       options: {
         htmlTitle: `Network360 CMS`,
+        // Decap CMS uses this path for two purposes, and uses it inconsistently:
+        // 1. It will place build artifacts in `public/${publicPath}`.
+        // 2. After a user logs in, it will navigate to `/${publicPath}/` (note the surrounding /'s).
+        // Setting `publicPath` to an empty string is what we want (so that CMS artifacts go in the root path).
+        // However, this causes Decap to try navigating to `//` (not a valid URL) which displays a big red
+        // error message and leaves the user on the login screen. Setting it to "<anything>/.."
+        // is functionally equivalent to the empty string and gets around the valid-URL problem.
         publicPath: "_/..",
       },
     },
