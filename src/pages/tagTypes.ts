987fdf436e9d1219a6c775d8ddcb586451fc6d8f
Noah Summers <nsmmrs@outlook.com>
2024-05-17 12:56:59 -0500

    ZNET-111 - Add Tag Resource

--- src/pages/tagTypes.ts
+++ src/pages/tagTypes.ts
@@ -4,14 +4,12 @@ const page = () => null;
 export default page;
 
 export const query = graphql`
-  query HelpFaqsQuery {
-    helpGlossary {
-      entries {
-        term
-        description
-        feature
+  query TagTypesQuery {
+    allTagType {
+      nodes {
+        title
         slug
-        tagSlugs
+        order
       }
     }
   }
