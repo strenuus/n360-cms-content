2e6adce65f2ca317293836b3496ee3e2d60987bf
Noah Summers <nsmmrs@outlook.com>
2024-07-19 15:20:31 -0400

    ZNET-595 - Include descriptions in all search results

--- src/pages/helpSections.ts
+++ src/pages/helpSections.ts
@@ -19,6 +19,7 @@ export const query = graphql`
         }
         feature
         tagSlugs
+        shortDescription
       }
     }
   }
--- src/pages/helpSubsections.ts
+++ src/pages/helpSubsections.ts
@@ -20,6 +20,7 @@ export const query = graphql`
         }
         feature
         tagSlugs
+        shortDescription
       }
     }
   }
